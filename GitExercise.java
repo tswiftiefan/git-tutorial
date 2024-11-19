42244d1742f5b4599b460ddce695b545079600e7
Jodi <77014748+fatsnorcat@users.noreply.github.com>
2024-11-18 17:29:17 -0800

    Update GitExercise.java

--- GitExercise.java
+++ GitExercise.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class GitExercise {
     public static void main(String[] args) {
         int elements;
-        int numbers[];
         String words[];
 
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the number of elements");
         elements = s.nextInt();
-        numbers = new int[elements];
+        words = new String[elements];
 
-        System.out.println("Enter " + elements + " numbers");
+        System.out.println("Enter " + elements + " words");
         for (int i=0; i < elements; i++){
-            numbers[i] = s.nextInt();
+            words[i] = s.next();
         }
 
-        System.out.print("You entered: ");
-        for (int num : numbers) {
-            System.out.print(num + " ");
+        System.out.print("\nYou entered: ");
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+
+        Arrays.sort(words);
+
+        System.out.print("\nWords in alphabetical order: ");
+        for (String word : words) {
+            System.out.print(word + " ");
         }
     }
 }
